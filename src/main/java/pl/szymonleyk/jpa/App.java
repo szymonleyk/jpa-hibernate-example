f2ec749bf99549adce982e571db066744ea69e44
Szymon Leyk <leyas@wp.pl>
2020-04-03 10:06:33 +0200

    ustawienie nazwy persistence unit, oraz dodanie przykładów wywołania

--- src/main/java/pl/szymonleyk/jpa/App.java
+++ src/main/java/pl/szymonleyk/jpa/App.java
@@ -4,10 +4,17 @@ package pl.szymonleyk.jpa;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		AddressRepository addressRepository = new AddressRepository();
+
+		// dodanie elementu
+		Address address = new Address("Gdynia", "Hutnicza", "3", 5, "51-222");
+		addressRepository.insert(address);
+
+		// pobierz element
+		Address szukanyAdres = new Address();
+		szukanyAdres = addressRepository.select(7);
+		System.out.println(szukanyAdres);
+	}
 }
